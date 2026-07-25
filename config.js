52a3007d022401feae007f490172c23ff8d9c58a
nerra1999 <nerkoic@gmail.com>
2026-07-25 09:06:33 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Supabase-Zugangsdaten für KaufDeinBike
+const SUPABASE_URL = "https://zpdwbwiqyusvlhxcjudf.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_flvwAkg355mRcGS6HzfIGQ_oh5Kwo3Y";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
